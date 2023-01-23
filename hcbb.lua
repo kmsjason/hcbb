f3a224f2a32b2102edd6f7d935b230820fe59813
Ethan <ethannwaike@gmail.com>
2023-01-23 00:08:43 -0600

    Disable tweening

--- hcbb.lua
+++ hcbb.lua
@@ -15,7 +15,7 @@ local set = {
 	AimWithMouse = true, --Honestly not sure what this does
 	showBoundsAndPrediction = false, --Show ball prediction
 	showStrikezone = false, --Whether or not you want the strike zone to be shown at all times
-	tweenSpeed = .05, --Affects steady bonus
+	tweenSpeed = 0, --Affects steady bonus
 }
 
 -- Get player related variables
